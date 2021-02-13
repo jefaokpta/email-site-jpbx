ef43205b7781d010aa38973ae540cf0b30e6852e
jefaokpta <jefaokpta@hotmail.com>
2021-02-13 18:06:20 -0300

    app precisa obter a porta aleatoria do heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { sendEmail } from './email-service.js';
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -18,4 +18,4 @@ app.post('/email', cors(), (req, res) => {
     })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.listen(port, () => console.log(`App rodando na porta ${port}`))
